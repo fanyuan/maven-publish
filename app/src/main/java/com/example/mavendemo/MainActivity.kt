9f570d9fd5375d5ae6c2e5d0946bfd7df5b0c8ae
fly <abc@123.com>
2022-01-11 11:33:20 +0800

    代码的修改

--- app/src/main/java/com/example/mavendemo/MainActivity.kt
+++ app/src/main/java/com/example/mavendemo/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.mavendemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.demo01.Demo01
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Demo01.log("this is a maven test")
     }
 }
--- demo01/src/main/java/com/example/demo01/Demo01.java
+++ demo01/src/main/java/com/example/demo01/Demo01.java
@@ -1,4 +1,11 @@
 package com.example.demo01;
 
+import android.util.Log;
+
 public class Demo01 {
+    private static final String TAG = "log_debug";
+
+    public static void log(String msg){
+        Log.d(TAG,"log is printed :" + msg);
+    }
 }
